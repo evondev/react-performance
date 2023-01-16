4803a3edea872bf508f31a4c77d2894cdbee35d8
evondev <trananhtuan400@gmail.com>
2023-01-17 00:38:17 +0700

    optimize re-render

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,55 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 // 1. necessary re-render
 // 2. unnecessary re-render
+// 3. memozied
+const VeryHeavyComponent = ({ data }: { data?: number[] | number }) => {
+  console.log("VeryHeavyComponent1 render");
+  return (
+    <div className="grid grid-cols-12 gap-5">
+      {Array(2000)
+        .fill(0)
+        .map((item, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-center w-20 h-20 bg-blue-500"
+          >
+            {index + 1}
+          </div>
+        ))}
+    </div>
+  );
+};
+const VeryHeavyComponent2 = () => {
+  console.log("VeryHeavyComponent2 render");
+  return (
+    <div className="grid grid-cols-12 gap-5">
+      {Array(2000)
+        .fill(0)
+        .map((item, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-center w-20 h-20 bg-blue-500"
+          >
+            {index + 1}
+          </div>
+        ))}
+    </div>
+  );
+};
+const VeryHeavyComponentMemo = React.memo(VeryHeavyComponent);
 function App() {
+  const [counter, setCounter] = useState(0);
+  const data = useMemo(() => [1, 2, 3, 4, 5], []);
   return (
     <div className="App">
+      <span>{counter}</span>
+      <button
+        onClick={() => setCounter(counter + 1)}
+        className="p-3 text-white bg-blue-500 rounded-lg"
+      >
+        +
+      </button>
+      <VeryHeavyComponentMemo data={data}></VeryHeavyComponentMemo>
       {/* <ComponentWithMouseMove2
         left={<VeryHeavyComponent></VeryHeavyComponent>}
         right={<VeryHeavyComponent2 />}
@@ -62,39 +108,6 @@ const ComponentWithMouseMove2 = ({
     </div>
   );
 };
+// 4. Những giá trị không phải primitives value string boolean number: {} []
 
-const VeryHeavyComponent = () => {
-  console.log("VeryHeavyComponent1 render");
-  return (
-    <div className="grid grid-cols-12 gap-5">
-      {Array(2000)
-        .fill(0)
-        .map((item, index) => (
-          <div
-            key={index}
-            className="flex items-center justify-center w-20 h-20 bg-blue-500"
-          >
-            {index + 1}
-          </div>
-        ))}
-    </div>
-  );
-};
-const VeryHeavyComponent2 = () => {
-  console.log("VeryHeavyComponent2 render");
-  return (
-    <div className="grid grid-cols-12 gap-5">
-      {Array(2000)
-        .fill(0)
-        .map((item, index) => (
-          <div
-            key={index}
-            className="flex items-center justify-center w-20 h-20 bg-blue-500"
-          >
-            {index + 1}
-          </div>
-        ))}
-    </div>
-  );
-};
 export default App;
